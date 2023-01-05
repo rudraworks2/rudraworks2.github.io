9dae47000d984651ea44c298bbe12b0b1254b423
rudraworks2 <merudra7542@gmail.com>
2023-01-06 00:06:51 +0530

    fix

--- features.js
+++ features.js
@@ -1,1 +1,10 @@
 // alert(chptr.textContent+","+ind)
+setTimeout(() => {
+    try{
+        document.getElementById('extra2bottom').style.fontSize="12px"
+        }
+        catch(e){
+            console.log(e)
+        }
+        
+}, 200);
